fbdfee24f5a1407cc239c3ddcbedc242885314e3
Samuele Ceroni <samueleceroni@gmail.com>
2018-07-10 14:54:17 +0200

    added segtreesum data structure

--- segTreeSum/segTreeSum.cpp
+++ segTreeSum/segTreeSum.cpp
@@ -7,16 +7,19 @@ using namespace std;
 struct SumSegmentTree {
     private:
     vector<int> v; // vettore che contiene l'albero
+    vector<int> u;
     int n; //numero nodi interni
     const int NULL_VALUE = 0; //zero dell'operazione
     public:
     SumSegmentTree(int num) {
         v.resize((1 << (int)(ceil(log2(num))+1))-1, NULL_VALUE);//completo bilanciato
+        u.resize((1 << (int)(ceil(log2(num))+1))-1, NULL_VALUE);//completo bilanciato
         n = v.size() / 2;
     }
 
     SumSegmentTree(vector<int> &source) {
         v.resize((1 << (int)(ceil(log2(source.size()))+1))-1, NULL_VALUE);//completo bilanciato
+        u.resize((1 << (int)(ceil(log2(source.size()))+1))-1, NULL_VALUE);//completo bilanciato
         n = v.size() / 2;
         for (int i = 0; i < source.size(); i++) {
             v[i + n] = source[i];
@@ -43,16 +46,26 @@ struct SumSegmentTree {
     }
 
     void updateNode(int i) {
-        v[i] = mergeValues(v[left(i)], v[right(i)]);
+        if(!isLeave(i))
+            v[i] = mergeValues(v[left(i)], v[right(i)]);
     }
 
     int rangeQuery(int i, int l, int r, int a, int b) {
-        if (l >= a && r <= b) {
-            return v[i];
-        }
         if (r < a || l > b) {
             return NULL_VALUE;
         }
+
+        v[i] += u[i];
+        if(!isLeave(i)){
+            u[(left(i))] += u[i]/2;
+            u[(right(i))] += u[i]/2;
+        }
+        u[i] = NULL_VALUE;
+
+        if (l >= a && r <= b) {
+            return v[i];
+        }
+
         int m = (l + r) / 2;
         return mergeValues(rangeQuery(left(i), l, m, a, b), rangeQuery(right(i), m + 1, r, a, b));
     }
@@ -63,7 +76,39 @@ struct SumSegmentTree {
             updateToRoot(parent(i));
         }
     }
+
+    bool isLeave(int i){
+        return i>v.size()/2;
+    }
+
+    void rangeUpdateQuery(int i, int l, int r, int a, int b, int value){
+
+        if (l >= a && r <= b){
+            if (!isLeave(i)){
+                u[left(i)]=value * (r-l+1)/2;
+                u[right(i)]=value * (r-l+1)/2;
+            }
+            v[i] += value * (r-l+1);
+            updateToRoot(parent(i));
+            return;
+        }
+        if (r<a || l>b){
+            return;
+        }
+        //v[i] += value * (b-a+1); // add to current node the value *number of leaves in the range of nodes i'll have to update
+        int m = (l + r) / 2;
+        rangeUpdateQuery(left(i), l, m, a, b, value);
+        rangeUpdateQuery(right(i), m + 1, r, a, b, value);    
+        
+    }
+
+
     public:
+    
+    void rangeUpdate(int a, int b, int value){
+        rangeUpdateQuery(0,0, n, a, b, value);
+    }
+
 
     int rangeSum(int a, int b) {
         return rangeQuery(0, 0, n, a, b);
@@ -80,7 +125,7 @@ struct SumSegmentTree {
         int livNum = 1;
         while (i < v.size()) {
             for (int j = 0; j < livNum; j++) {
-                cout << v[i] << " ";
+                cout << v[i] << " (" <<u[i]<<") ";
                 i++;
             }
             livNum *= 2;
@@ -88,3 +133,25 @@ struct SumSegmentTree {
         }
     }
 };
+
+int main() { // main di test della struttura
+    vector<int> v;
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(7);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(1);
+    SumSegmentTree st(v);
+    st.print();
+    cout << st.rangeSum(0, 3) << ' ' << st.rangeSum(2, 5) << '\n';
+    st.update(3, 5);
+    st.print();
+    cout << st.rangeSum(0, 3) << ' ' << st.rangeSum(2, 5) << '\n';
+    st.rangeUpdate(3, 5, 100);
+    st.print();
+    cout << st.rangeSum(0, 3) << ' ' << st.rangeSum(2, 5) << '\n';
+    
+
+    return 0;
+}
