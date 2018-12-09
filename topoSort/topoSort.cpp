d05562fac798a2832a0364b4d156ed1a4d906f49
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-12-09 16:18:19 +0100

    topoSort new version

--- topoSort/topoSort.cpp
+++ topoSort/topoSort.cpp
@@ -1,65 +1,47 @@
 #include<iostream>
 #include<vector>
-#include<list>
-#include<unordered_set>
+#include<algorithm>
 using namespace std;
 
-struct Node{
-    int data; // could be whatever, also NULL
-    vector<int> childs;
-    int mark; // 0 not Visited, 1 temp, 2 perm
-};
-
-struct Graph{
-    vector<Node> nodes;
-};
-
-bool visit(int currNode, Graph &g, list<int> &sol, unordered_set<int> &unmarked){
-    if(g.nodes[currNode].mark == 2){return;}
-    if(g.nodes[currNode].mark == 1){return false;} // not a DAG
-    g.nodes[currNode].mark = 1;
-    unmarked.erase(currNode);
-
-    for(auto n : g.nodes[currNode].childs){
-        visit(n, g, sol, unmarked);
+bool visit(int currNode, const vector<vector<int> > & g, vector<int> &sol, vector<int> &marks){
+    if(marks[currNode] == 2){return true;}
+    if(marks[currNode] == 1){return false;} // not a DAG
+    marks[currNode] = 1;
+    for(auto nextNode : g[currNode]){
+        bool ok = visit(nextNode, g, sol, marks);
+        if(!ok){return false;}
     }
-    g.nodes[currNode].mark = 2;
-    sol.push_front(currNode);
+    marks[currNode] = 2;
+    sol.push_back(currNode);
     return true;
 }
 
-list<int> topoSort(Graph &g){
-    unordered_set<int> unmarked;
-    list<int> sol;
-    for(int i=0; i<g.nodes.size(); i++){
-        unmarked.insert(i);
+vector<int> topoSort(const vector<vector<int> > &g){
+    vector<int> marks(g.size(), 0);
+    vector<int> sol;
+    for(auto nextEl : marks){
+        if(!visit(nextEl, g, sol, marks)){return vector<int>();}
     }
-    while(!unmarked.empty()){
-        int nextEl = *unmarked.begin();
-        if(!visit(nextEl, g, sol, unmarked)){return list<int>();}
-    }
+    reverse(sol.begin(), sol.end());
     return sol;
 }
 
-int main(){
-    Graph g;// = new Graph;
-    int n = 5;
-    for(int i = 0; i < n; i++){
-        Node n;// = new Node;
-        //n.data = i;
-        n.mark = 0;
-        g.nodes.push_back(n);
+int main()
+ {
+    int n, e;
+    int start, end;
+    cin >> n >> e;
+
+    vector<vector<int> > g(n);
+    for(int i = 0; i < e; i++){
+        cin >> start >> end;
+        g[start].push_back(end);
     }
-    g.nodes[0].childs.push_back(1);
-    g.nodes[0].childs.push_back(2);
-    g.nodes[0].childs.push_back(3);
-    g.nodes[1].childs.push_back(4);
-    g.nodes[2].childs.push_back(4);
-    g.nodes[3].childs.push_back(2);
-    g.nodes[3].childs.push_back(4);
-    
-    list <int> t = topoSort(g);
-    for(int i : t){
-        cout << i << ' ';
+    vector<int> topoSorted = topoSort(g);
+
+    for(int el : topoSorted){
+        cout << el << ' ';
     }
+    cout << endl;
+    return 0;
 }
